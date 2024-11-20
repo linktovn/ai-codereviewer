84236d5ee60a6012af94ad03b63301630a0cca74
steven <congnguyen@Congs-Mac-mini.local>
2024-11-20 14:59:45 +0700

    Complete

--- src/main.ts
+++ src/main.ts
@@ -180,15 +180,33 @@ async function createReviewComment(
 async function main() {
   const prDetails = await getPRDetails();
   let diff: string | null;
-
   const eventData = JSON.parse(
     readFileSync(process.env.GITHUB_EVENT_PATH ?? "", "utf8")
   );
 
   if (eventData.action === "opened") {
-    diff = await getDiff(prDetails.owner, prDetails.repo, prDetails.pull_number);
+    diff = await getDiff(
+      prDetails.owner,
+      prDetails.repo,
+      prDetails.pull_number
+    );
+  } else if (eventData.action === "synchronize") {
+    const newBaseSha = eventData.before;
+    const newHeadSha = eventData.after;
+
+    const response = await octokit.repos.compareCommits({
+      headers: {
+        accept: "application/vnd.github.v3.diff",
+      },
+      owner: prDetails.owner,
+      repo: prDetails.repo,
+      base: newBaseSha,
+      head: newHeadSha,
+    });
+
+    diff = String(response.data);
   } else {
-    console.log("Unsupported event");
+    console.log("Unsupported event:", process.env.GITHUB_EVENT_NAME);
     return;
   }
 
@@ -198,6 +216,17 @@ async function main() {
   }
 
   const parsedDiff = parseDiff(diff);
+
+  const excludePatterns = core
+    .getInput("exclude")
+    .split(",")
+    .map((s) => s.trim());
+
+  const filteredDiff = parsedDiff.filter((file) => {
+    return !excludePatterns.some((pattern) =>
+      minimatch(file.to ?? "", pattern)
+    );
+  });
   const comments = await analyzeCode(parsedDiff, prDetails);
 
   if (comments.length > 0) {
