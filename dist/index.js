b689379d4ae67befe23e873698fb11684daf6633
Steven Nguyen <cong.nguyen@a5labs.co>
2024-11-20 21:08:49 +0700

    Complete

--- dist/index.js
+++ dist/index.js
@@ -425,13 +425,31 @@ function analyzeCode(parsedDiff, prDetails) {
 }
 function createPrompt(file, chunk, prDetails) {
     const defaultPrompt = `Your task is to review pull requests. Instructions:
-                        - Provide comments and suggestions ONLY if there is something to improve.
-                        - Provide the response in JSON format: {"reviews": [{"lineNumber": <line_number>, "reviewComment": "<review comment>"}]}`;
+                        - Do not give positive comments or compliments.
+                        - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
+                        - Write the comment in GitHub Markdown format.
+                        - Use the given description only for the overall context and only comment the code.
+                        - IMPORTANT: NEVER suggest adding comments to the code.
+                        - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}`;
     const prompt = CUSTOM_PROMPT ? CUSTOM_PROMPT : defaultPrompt;
     return `${prompt}
-Review the following code diff in the file "${file.to}" with the PR title "${prDetails.title}":
+Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
+  
+Pull request title: ${prDetails.title}
+Pull request description:
+
+---
+${prDetails.description}
+---
+
+Git diff to review:
+
 \`\`\`diff
 ${chunk.content}
+${chunk.changes
+        // @ts-expect-error - ln and ln2 exists where needed
+        .map((c) => `${c.ln ? c.ln : c.ln2} ${c.content}`)
+        .join("\n")}
 \`\`\`
 `;
 }
